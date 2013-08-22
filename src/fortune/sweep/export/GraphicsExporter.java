0ffe827ad2fe5c397b1bbd649a318c188d660197
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-08-22 19:58:29 +0200

    implemented PNG export

--- src/fortune/sweep/export/GraphicsExporter.java
+++ src/fortune/sweep/export/GraphicsExporter.java
@@ -1,5 +1,7 @@
 package fortune.sweep.export;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,6 +9,9 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import fortune.sweep.Algorithm;
+import fortune.sweep.gui.AlgorithmPainter;
+import fortune.sweep.gui.Config;
+import fortune.sweep.gui.swing.AwtPainter;
 
 public class GraphicsExporter
 {
@@ -17,7 +22,24 @@ public class GraphicsExporter
 		BufferedImage image = new BufferedImage(width, height,
 				BufferedImage.TYPE_4BYTE_ABGR);
 
-		// TODO implement this
+		Graphics2D graphics = image.createGraphics();
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+
+		AwtPainter painter = new AwtPainter(graphics);
+
+		Config config = new Config();
+		config.setDrawBeach(true);
+		config.setDrawCircles(true);
+		config.setDrawDelaunay(false);
+		config.setDrawVoronoiLines(true);
+
+		AlgorithmPainter algorithmPainter = new AlgorithmPainter(algorithm,
+				config, painter);
+
+		algorithmPainter.setWidth(width);
+		algorithmPainter.setHeight(height);
+		algorithmPainter.paint();
 
 		ImageIO.write(image, "png", file);
 	}
